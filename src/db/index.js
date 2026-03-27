b9f898e3fde17e9a3d8b8eb792f7c0adc8ee4190
sidtric <siddharthfuloria06@gmail.com>
2026-03-27 15:43:40 +0530

    database connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants/index.js";
+
 
 const connectDB = async ()=>{
     try{
-        const connection = await mongoose.connect(`${process.env.DATABASE_URI}/${DB_NAME}`)
+        const connection = await mongoose.connect(`${process.env.DATABASE_URI}`)
         console.log('\n mongo db connected')
-        console.log(connection)
+       
         
 
     }catch(err){
